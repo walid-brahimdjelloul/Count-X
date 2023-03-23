124241e7f8f8cd7d2ab560a93e465bb05fa30f89
walid brahimdjelloul <103107369+walid-brahimdjelloul@users.noreply.github.com>
2023-03-24 00:02:26 +0100

    Add files via upload

---
+++ 2. Count X.py
@@ -0,0 +1,13 @@
+## Next, we are going to try something a bit different. This time we are going to count the number of occurrences of a certain letter within a string. Our function will accept a parameter for a string and another for the character which we are going to count. For example, providing the word "mississippi" and the character 's' would result in an answer of 4.
+# Write your count_char_x function here:
+def count_char_x(word, x):
+  tot = 0
+  for i in word:
+    if i == x:
+      tot += 1
+  return tot
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
